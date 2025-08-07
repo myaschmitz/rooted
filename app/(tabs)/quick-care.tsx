3a2f6adb35c1e589d1336164ee723b992e187442
Mya Schmitz <myaschm@gmail.com>
2025-08-06 18:42:49 -0700

    background update

--- app/(tabs)/quick-care.tsx
+++ app/(tabs)/quick-care.tsx
@@ -672,7 +672,7 @@ const createStyles = (theme) => StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: theme.colors.border,
     marginBottom: 8,
-    backgroundColor: theme.colors.surface,
+    backgroundColor: theme.colors.background,
   },
   locationTitle: {
     fontSize: 18,
