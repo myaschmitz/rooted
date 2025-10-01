88d4e397c2447f5643aa7c3ddb0324190bd8cee7
Mya Schmitz <myaschm@gmail.com>
2025-10-01 14:37:23 -0700

    fixed dependencies

---
+++ app.config.js
@@ -0,0 +1,53 @@
+export default ({ config }) => {
+  const bundleSuffix = process.env.BUNDLE_SUFFIX || '';
+  
+  return {
+    ...config,
+    expo: {
+      name: "Rooted",
+      slug: "rooted",
+      version: "1.0.0",
+      sdkVersion: "54.0.0",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      android: {
+        package: `com.myaschm.rooted${bundleSuffix}`,
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#ffffff"
+        }
+      },
+      userInterfaceStyle: "automatic",
+      assetBundlePatterns: [
+        "**/*"
+      ],
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: `com.myaschm.rooted${bundleSuffix}`,
+        infoPlist: {
+          ITSAppUsesNonExemptEncryption: false,
+          NSCameraUsageDescription: "This app needs access to your camera to take photos of your plants.",
+          NSPhotoLibraryUsageDescription: "This app needs access to your photo library to select photos of your plants."
+        }
+      },
+      web: {},
+      plugins: [
+        "expo-router",
+        [
+          "expo-sqlite",
+          {
+            enableFTS: true
+          }
+        ]
+      ],
+      scheme: "rooted",
+      extra: {
+        router: {},
+        eas: {
+          projectId: "c985d2e8-93a8-4a5d-a7b7-cb59d9e46f11"
+        }
+      },
+      owner: "myaschm"
+    }
+  };
+};
