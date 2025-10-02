a02e408b25b9e3812e7b68f859229feea3ecad08
Mya Schmitz <myaschm@gmail.com>
2025-10-02 10:43:08 -0700

    photo service fix

--- services/CachedPhotoService.ts
+++ services/CachedPhotoService.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { Image } from 'expo-image';
 import { PlantPhoto } from '../types/Plant';
 import { CacheService } from './CacheService';
